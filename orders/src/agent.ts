6327d0dd0355bdfd1752ddb72fe7059479c58847
Mohd Tarmizi <mohtar@users.noreply.github.com>
2020-08-06 23:05:04 +0800

    Payments URL

--- orders/src/agent.ts
+++ orders/src/agent.ts
@@ -1,6 +1,7 @@
 import * as orders from './orders';
 import axios from 'axios';
 import redis from 'redis';
+import url from 'url';
 
 const client = redis.createClient(process.env.REDIS_URL);
 
@@ -9,7 +10,10 @@ client.on('message', async (_chan, message) => {
   const {order} = JSON.parse(message);
   if (order.status == 'created') {
     const {currency, amount} = order;
-    const response = await axios.post('http://payments/', {currency, amount});
+    const response = await axios.post(
+      url.resolve(process.env.PAYMENTS_URL, 'payments'),
+      {currency, amount},
+    );
     const transitionMap = new Map([
       ['confirmed', 'confirm'],
       ['declined', 'cancel'],
